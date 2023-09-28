7f65701464db0a2d7488abe74f6760dcd017548e
NicolasBiondini <nicolasbiondini29@gmail.com>
2023-09-28 19:17:19 -0300

    finished v2.2

--- src/components/Searcher.tsx
+++ src/components/Searcher.tsx
@@ -10,7 +10,9 @@ type Props = {
 const regex =
   /^(?:[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}|[a-zA-Z0-9-]+\.eth|0x[a-fA-F0-9]{40})$/;
 
-const final_url = process.env.URL_BACKEND || "";
+const final_url =
+  process.env.URL_BACKEND ||
+  "https://main--helpful-bienenstitch-10f321.netlify.app/";
 
 function Searcher({ setPoaps, amount }: Props) {
   const [inputValue, setInputValue] = useState<{
